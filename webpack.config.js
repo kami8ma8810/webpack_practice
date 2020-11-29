dfecfa04772d86f19a11d1257d7a52cc4c97a5dc
Hayato Kamiyama <kami8ma8810@gmail.com>
2020-11-29 10:20:52 +0900

    出力先の設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const miniCssExtractPlugin = require('mini-css-extract-plugin');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const Path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
-  Template
-} = require('webpack');
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'), //絶対パスで書く設定
-    filename: 'main.js', //distに出力される名前の変更
+    path: Path.resolve(__dirname, './dist'), //絶対パスで書く設定
+    filename: 'js/main.js', //distに出力される名前の変更
   },
   module: {
     rules: [{
       test: /\.css/,
       //※loaderはuse内の下から順に実行される
       use: [{
-          loader: miniCssExtractPlugin.loader, //main.cssの作成
+          loader: MiniCssExtractPlugin.loader, //main.cssの作成
         },
         {
           loader: 'css-loader', //CSS読み込み
@@ -26,9 +26,12 @@ module.exports = {
     }, ],
   },
   plugins: [
-    new miniCssExtractPlugin(),
-    new htmlWebpackPlugin({
+    new MiniCssExtractPlugin({
+      filename: './style/my.css',
+    }),
+    new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
 }
