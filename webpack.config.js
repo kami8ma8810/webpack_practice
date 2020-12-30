7b898621447ed6ea7f68ec829d8cd45f2e76417f
Hayato Kamiyama <kami8ma8810@gmail.com>
2020-12-30 11:03:43 +0900

    pugの導入完了

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,54 @@ const Path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
-  CleanWebpackPlugin
+	CleanWebpackPlugin
 } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/javascripts/main.js',
-  output: {
-    path: Path.resolve(__dirname, './dist'), //絶対パスで書く設定
-    filename: 'javascripts/main.js', //distに出力される名前の変更
-  },
-  module: {
-    rules: [{
-      test: /\.css/,
-      //※loaderはuse内の下から順に実行される
-      use: [{
-          loader: MiniCssExtractPlugin.loader, //main.cssの作成
-        },
-        {
-          loader: 'css-loader', //CSS読み込み
-        },
-      ],
-    }, {
-      test: /\.(png|jpg)/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          esModule: false,
-          name: 'images/[name].[ext]',
-        },
-      }, ],
-    }],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: './stylesheets/main.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
+	entry: './src/javascripts/main.js',
+	output: {
+		path: Path.resolve(__dirname, './dist'), //絶対パスで書く設定
+		filename: 'javascripts/main.js', //distに出力される名前の変更
+	},
+	module: {
+		rules: [{
+			test: /\.css/,
+			//※loaderはuse内の下から順に実行される
+			use: [{
+					loader: MiniCssExtractPlugin.loader, //main.cssの作成
+				},
+				{
+					loader: 'css-loader', //CSS読み込み
+				},
+			],
+		}, {
+			test: /\.(png|jpg)/,
+			use: [{
+				loader: 'file-loader',
+				options: {
+					esModule: false,
+					name: 'images/[name].[ext]',
+				},
+			}, ],
+		}, {
+			test: /\.pug/,
+			use: [{
+				loader: 'html-loader',
+			}, {
+				loader: 'pug-html-loader',
+				options: {
+					pretty: true,
+				},
+			}, ],
+		}, ],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: './stylesheets/main.css',
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/templates/index.pug',
+		}),
+		new CleanWebpackPlugin(),
+	],
 }
